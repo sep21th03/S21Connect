984c046e7617e56e8edf836f2ccdc92f3c88235a
Sep 21th <nguyenhoathpt03@gmail.com>
2025-07-03 13:30:06 +0700

    fix

--- FE/src/components/Favorite/Home/ContentLeft/AboutCard.tsx
+++ FE/src/components/Favorite/Home/ContentLeft/AboutCard.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 import { Page } from "@/service/fanpageService";
 
 const AboutCard: FC<{ pageInfo: Page }> = ({ pageInfo }) => {
-
   const typeMap: Record<string, string> = {
     community: "Cộng đồng",
     brand: "Thương hiệu",
@@ -20,20 +19,17 @@ const AboutCard: FC<{ pageInfo: Page }> = ({ pageInfo }) => {
     { heading: "Trang", detail: typeMap[pageInfo?.type || ""] },
     {
       heading: "email",
-      detail:
-        pageInfo?.email,
+      detail: pageInfo?.email,
     },
     {
       heading: "phone",
       detail: pageInfo?.phone,
     },
-    { heading: "website"},
   ];
-
 
   return (
     <div className="profile-about pages-about">
-      <div className="card-title" style={{paddingBottom: "0px"}}>
+      <div className="card-title" style={{ paddingBottom: "0px" }}>
         <h3>{About}</h3>
         <div className="settings">
           <div className="setting-btn">
@@ -49,20 +45,31 @@ const AboutCard: FC<{ pageInfo: Page }> = ({ pageInfo }) => {
       <div className="about-content">
         <ul>
           <h3 className="text-center">{pageInfo?.description}</h3>
-          {aboutContentData.map((data, index) => (
-            <li key={index}>
+          {aboutContentData
+            .filter(
+              (data) =>
+                data.detail !== null &&
+                data.detail !== undefined &&
+                data.detail !== ""
+            )
+            .map((data, index) => (
+              <li key={index}>
+                <div className="details">
+                  <h4>{data.heading}</h4>
+                  <h6>{data.detail}</h6>
+                </div>
+              </li>
+            ))}
+          {pageInfo?.link && (
+            <li>
               <div className="details">
-                <h4>{data.heading}</h4>
+                <h4>website</h4>
                 <h6>
-                  {data.detail ? (
-                    data.detail
-                  ) : (
-                    <Link href={`${pageInfo?.link}`}>{pageInfo?.link}</Link>
-                  )}
+                  <Link href={pageInfo.link}>{pageInfo.link}</Link>
                 </h6>
               </div>
             </li>
-          ))}
+          )}
         </ul>
       </div>
     </div>
